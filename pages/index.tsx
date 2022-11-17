5e07e6f93fdc58e9232da67ea7ebb43543776f2b
Andrew Wint <109804064+awint-ah@users.noreply.github.com>
2022-11-17 15:21:08 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{' '} Hello Next.js 13
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
